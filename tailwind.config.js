907b5e3aa920a8b9e66256a188510edc841846ba
faezehdev <faezeh0078@gmail.com>
2024-08-11 19:00:48 +0330

    change config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,38 @@ module.exports = {
     './js/**/*.js',
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'orange': '#F26737',
+      'blue': '#2E3192F3',
+      'yellow': '#F9CF66',
+      'black-1': '#1E1E1E',
+      'gray-light': '#D9D9D9',
+      'white-1': '#FBFBFF',
+      'gray-dark': '#BCBDBD',
+      'blue-gray':'#1E293B',
+      'gray-1':'#64748B',
+      'white-2':'#FAFAFA',
+      'gray-2':'#E4E4E7',
+    },
+    fontFamily: {
+      // sans: ['Graphik', 'sans-serif'],
+      // serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      // spacing: {
+      //   '128': '32rem',
+      //   '144': '36rem',
+      // },
+      // borderRadius: {
+      //   '4xl': '2rem',
+      // }
+    }
   },
   plugins: [
     plugin(function({ addComponents }) {
