f8ccf1d22e3182802761e26f80e39e18d9270fbb
faezehdev <faezeh0078@gmail.com>
2024-08-11 15:09:16 +0330

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+module.exports = {
+  content: [
+    './**/*.{html,js}',
+    './css/**/*.css',
+    './js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function({ addComponents }) {
+      addComponents({
+        '.btn': {
+          padding: '.5rem 1rem',
+          borderRadius: '.25rem',
+          fontWeight: '600',
+        },
+        '.btn-blue': {
+          backgroundColor: '#3490dc',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#2779bd'
+          },
+        },
+        '.btn-red': {
+          backgroundColor: '#e3342f',
+          color: '#fff',
+          '&:hover': {
+            backgroundColor: '#cc1f1a'
+          },
+        },
+      })
+    })
+  ]
+}
