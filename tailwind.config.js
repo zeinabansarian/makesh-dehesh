dd12edbc0b230e99675f5694fc3b9ea117fafe6d
faezehdev <faezeh0078@gmail.com>
2024-08-13 13:33:33 +0330

    complete listproject page , footer and animation header

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
       'b3':'#E4E4E7',
       'b4':'#FAFAFA',
       'b6': '#2E3192F3',
+      'b7':'#2E3192',
       'b8': '#F26737',
       'b9': '#1E1E1E',
       'b10': '#D9D9D9',
@@ -34,6 +35,8 @@ module.exports = {
       'IRANSansWeb400': ['IRANSansWeb400', 'IRANSansWeb400'],
       'IRANSansWeb500': ['IRANSansWeb500', 'IRANSansWeb500'],
       'IRANSansWeb700': ['IRANSansWeb700', 'IRANSansWeb700'],
+      'YekanBakh400':[' YekanBakh400'],
+      'YekanBakh700':[' YekanBakh700']
     },
     extend: {
       spacing: {
@@ -51,7 +54,8 @@ module.exports = {
         '25.4':'25.04px',
         '57.97':'57.97px',
         '17.96': '17.96px',
-        '21.91': '21.91px'
+        '21.91': '21.91px',
+        '32.03':'32.03px'
 
 
       },
@@ -63,6 +67,7 @@ module.exports = {
         'x100': '100%',
       },
       fontSize: {
+        'f1':'20px',
         'xl':'24px',
         '2xl': '64px',
 '        3xl': '1.953rem',
