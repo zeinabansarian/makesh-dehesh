73afab996e3a8d41909a736cb244c321c632b530
faezehdev <faezeh0078@gmail.com>
2024-08-13 15:00:18 +0330

    complete section one of project page

---
+++ js/project.js
@@ -0,0 +1,6 @@
+// shareBox
+let ShareB = document.querySelector('.ShareB')
+let ShareBox = document.querySelector('.ShareBox')
+ShareB.addEventListener('click',()=>{
+    ShareBox.classList.toggle('show')
+})
