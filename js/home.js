b5bba38afb958ba1827531d244b6946578dc79ae
Zeinab <zeinabansarian6280@gmail.com>
2024-08-17 19:43:33 +0330

    complete some sections of home page

--- js/home.js
+++ js/home.js
@@ -40,6 +40,53 @@ const relatedPro = new Swiper('.relatedPro', {
   });
 
 
+  const thumbSlider = new Swiper('.thumbSlider', {
+
+    slidesPerView :3,
+    // Navigation arrows
+  
+
+
+  });
+const rSlider = new Swiper('.rSlider', {
+
+    slidesPerView : 1,
+    speed:1000,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.serNextBtn',
+      prevEl: '.serPrebtn',
+    },
+  
+    thumbs: {
+      swiper: thumbSlider
+    }
+
+  });
+const newsSlider = new Swiper('.newsSlider', {
+  spaceBetween : 12,
+  slidesPerView : 3,
+
+    speed:1000,
+    // Navigation arrows
+  navigation: {
+    nextEl: '.nextNewsBtn',
+    prevEl: '.prevNewsBtn',
+  },
+
+
+  });
+
+
+let listSlider = document.querySelectorAll(".listSlider")
+listSlider[0].classList.add("activeList")
+listSlider.forEach((element , index) => {
+    element.addEventListener("click" , function (params) {
+      rSlider.slideTo(index,1000);
+      $(".listSlider").removeClass("activeList")
+      element.classList.add("activeList")
+    })
+});
 
 // if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
 //     document.documentElement.classList.add('dark')
