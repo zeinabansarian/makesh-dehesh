35739d6a3e9170ddf8c9356f4e1206eba850d1d3
faezehdev <faezeh0078@gmail.com>
2024-08-17 12:18:04 +0330

    complete galleryshow img

--- js/galleryShow.js
+++ js/galleryShow.js
@@ -1,24 +1,124 @@
 let sideBarLi = document.querySelectorAll(".sidebar li")
 
 sideBarLi[0].classList.add("activeList")
-
+//  POPUP
+let gallerys
 sideBarLi.forEach(element => {
     element.addEventListener("click" , function(params) {
-        
+        console.log(params.currentTarget.getAttribute('id'));
+        if(params.currentTarget.getAttribute('id')=='img')
+        {
+            tabs[1].classList.remove('show') 
+            tabs[0].classList.add('show') 
+            console.log('imggg');
+            popupIMG()
+        }
+        else
+            {
+                tabs[0].classList.remove('show')
+                tabs[1].classList.add('show') 
+                popupVideo()
+            }
         $(".sidebar li").removeClass("activeList")
         element.classList.add("activeList")
     })
 });
+let tabs = document.querySelectorAll('.tab')
+tabs[0].classList.add('show')
 
-// VIDEO POPUP
-let gallerys = document.querySelectorAll('.VideoBox')
-let h =0
+
+function popupIMG(){
+
+    gallerys = document.querySelectorAll('.Gallery')
+    let h =0
 let Is = true
 gallerys.forEach(g=>{
     g.setAttribute(`data`,h)
     h++
     let clickedIMG =''
     g.addEventListener('click',(e)=>{
+         document.querySelector('img.Main').style.display='flex'
+document.querySelector('.VideoMain .Video ').style.display='none'
+         clickedIMG =Number(e.currentTarget.getAttribute('data'))
+         console.log('clicked',clickedIMG);
+        let popup = document.querySelector('.Gallerypopup')
+        popup.classList.add('showPOPUP')
+        let MimgSCR = e.currentTarget.querySelector('img').getAttribute('src')
+        console.log(MimgSCR);
+        let mainIMG = document.querySelector('img.Main')
+        mainIMG.setAttribute('src',MimgSCR)
+        mainIMG.classList.add('active')
+        let  Others=[]
+        for(let i = 0 ; i< gallerys.length; i++){
+         if(gallerys[i].getAttribute('data') != clickedIMG ){
+            clickActive = gallerys[i]
+         }       
+         Others.push(gallerys[i])
+        }
+        let previewIMG = document.querySelector(' img.Main')
+        let newIndex = clickedIMG 
+        function preview(){
+            let i = clickedIMG
+            let selectedIMGurl = gallerys[newIndex].querySelector('img') //get user clicked img url
+            console.log('sel',gallerys[i],i);
+            console.log('indec',newIndex);
+            previewIMG.classList.remove('active')
+            setTimeout(()=>{
+                previewIMG.setAttribute('src',selectedIMGurl.getAttribute('src'))
+                previewIMG.classList.add('active')
+            },200)
+
+            }
+      // previos next btn
+      let nextBtn = document.querySelector('.Gnext')
+      let prevBtn =document.querySelector('.Gprev')
+      nextBtn.onclick=()=>{
+      newIndex++
+      console.log('glen',gallerys.length);
+      console.log('in',newIndex);
+      if(newIndex == gallerys.length){
+
+      newIndex = 0
+      preview()
+      }
+      else{
+      preview() //call aboce function to update image
+      }
+      }
+      prevBtn.onclick=()=>{
+        console.log('in',newIndex);
+      newIndex--
+      if(newIndex < 0){
+        newIndex = gallerys.length-1
+        preview()
+      }
+      else{
+      preview() //call aboce function to update image
+      }
+      }
+         Is = false
+        let close = document.querySelector('.close')
+        close.addEventListener('click',()=>{
+        let popup = document.querySelector('.Gallerypopup')
+        popup.classList.remove('showPOPUP')
+        })
+    })
+})
+}
+popupIMG()
+
+function popupVideo(){
+    console.log('videoo');
+    gallerys = document.querySelectorAll('.VideoBox')
+    let h =0
+let Is = true
+gallerys.forEach(g=>{
+    g.setAttribute(`data`,h)
+    h++
+    let clickedIMG =''
+    g.addEventListener('click',(e)=>{
+        document.querySelector('.VideoMain .Video ').style.display='flex'
+          document.querySelector('img.Main').style.display='none'
          clickedIMG =Number(e.currentTarget.getAttribute('data'))
          console.log('clicked',clickedIMG);
         let popup = document.querySelector('.Gallerypopup')
@@ -114,3 +214,4 @@ gallerys.forEach(g=>{
         })
     })
 })
+}
