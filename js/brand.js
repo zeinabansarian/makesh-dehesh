f755671bf82a317e78e7c2829586962fd3caf201
Zeinab <zeinabansarian6280@gmail.com>
2024-08-18 16:05:02 +0330

    complete brand show page

---
+++ js/brand.js
@@ -0,0 +1,12 @@
+const relatedPro = new Swiper(".relatedPro", {
+    // Optional parameters
+    // loop: true,
+    spaceBetween: 52,
+    slidesPerView: 3,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".nextProBtn",
+      prevEl: ".prevProBtn",
+    },
+  });
